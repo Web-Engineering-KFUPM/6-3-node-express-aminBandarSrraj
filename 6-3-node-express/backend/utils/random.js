5ea777a0188bb04a4e84ec83bf3cd6d4da04f085
aminBandarSrraj <s202260140@kfupm.edu.sa>
2026-04-06 08:58:55 +0300

    final correction

--- 6-3-node-express/backend/utils/random.js
+++ 6-3-node-express/backend/utils/random.js
@@ -1,7 +1,7 @@
 // TODO 2: Create & Implement getRandomInt function
 
  function getRandomInt (max){
-     return Math.floor(Math.random * max);
+     return Math.floor(Math.random() * max);
 }
 
 export default getRandomInt;
